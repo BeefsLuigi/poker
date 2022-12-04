4bfd12105ff2bd19d37d0752ece16f1cea317024
BeefsLuigi <115431553+BeefsLuigi@users.noreply.github.com>
2022-12-04 11:26:01 -0500

    git commit

--- main.py
+++ main.py
@@ -7,24 +7,29 @@ import hand
 import scoring
 
 
-deck = plain_52.create_deck()
-random.shuffle(deck)
+def test():
 
-names = ['Steve', 'Amber', 'Jim', 'Mike', 'Goku']
+    deck = plain_52.create_deck()
+    random.shuffle(deck)
 
-players = []
+    names = ['Jeanee', 'Bob', 'Allen', 'Sam', 'Bryan']
 
-for i in names:
-    players.append(hand.player(deck, i))
+    players = []
+
+    for i in names:
+        players.append(hand.player(deck, i))
 
 
-winner = scoring.judge(players)
+    winner = scoring.judge(players)
 
-for i in players:
-    i.show_hand()
+    for i in players:
+        i.show_hand()
 
-print('the winner is ' + winner.player_name + ' with a ' + winner.hand_type)
+    print('the winner is ' + winner.player_name + ' with a ' + winner.hand_type)
 
-#print ('score: ' + str(hand1.score))
-#print ('hand type: ' + hand1.hand_type)
-#print ('highcard: ' + hand1.highcard.name)
+
+
+test()
+
+
+
